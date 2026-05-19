c80e98aa67f13391dbff3767de54f5a579ce823a
Arin Sharma <arinsharma57@gmail.com>
2026-05-19 19:36:21 +0530

    Updated POrt

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,11 +10,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 8000;
+
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-app.set("port", process.env.PORT || 8000);
+app.set("port", PORT);
 app.use(cors()); // Cross Origin Resource sharing ...(To accept req from diff origin or domain)
 app.use(express.json({ limit: "40kb" })); //Parse the req.body to readable obj.
 app.use(express.urlencoded({ limit: "40kb", extended: true })); // Parses URL-encoded form data ( e.g. HTML forms with POST)
